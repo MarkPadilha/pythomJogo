5f5cf9d303e1a35f2167af6acbf3dadf8845cf81
Mark Padilha <markpadilha10@gmail.com>
2023-10-27 17:42:21 -0300

    jogo forca em desenvolvimento

--- forca.py
+++ forca.py
@@ -6,6 +6,23 @@ def jogar():
     print("** Bem vindo ao jogo da forca **")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+
+        for letra in palavra_secreta:
+            if(letra.upper() == chute.upper()):
+                print("Tem a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
     print("Fim de jogo!")
 
 if(__name__ == "__main__"):
